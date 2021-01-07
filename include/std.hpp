3b3bc178e76cdf5068b9423cd15d8d7f57b5eb7b
Jerry Ma <jerrymax.jm@outlook.com>
2021-01-07 23:10:33 +0800

    fix some bugs

--- include/std.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef STD_HPP
-#define STD_HPP
-
-// only use this .hpp file in main.cpp file
-#include <iostream>
-#include <optional>
-#include <vector>
-
-#endif
--- src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "observer_pattern/subject.hpp"
-#include "strategy_pattern/duck.hpp"
-
-int main() {
-    design_patterns::testStrategyPattern();
-    design_patterns::testObserverPattern();
-    return 0;
-}
